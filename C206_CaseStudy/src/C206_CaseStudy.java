84885f55ba7e17dbbc401504d5d20446fe23223d
20009053 <20009053@myrp.edu.sg>
2021-08-04 17:14:13 +0800

    menu stuff

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -1,9 +1,57 @@
+import java.util.ArrayList;
 
 public class C206_CaseStudy {
+	
+	private static final int OPTION_QUIT = 5;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		ArrayList<Tuition> tuitionList = new ArrayList<Tuition>();
 
+		int option = 0;
+
+		while (option != OPTION_QUIT) {
+
+			C206_CaseStudy.menu();
+			option = Helper.readInt("Enter an option > ");
+
+			if (option == 1) {
+
+
+			} else if (option == 2) {
+
+
+			} else if (option == 3) {
+
+
+			} else if (option == 4) {
+
+
+			} else if (option == OPTION_QUIT) {
+				System.out.println("Bye!");
+			} else {
+				System.out.println("Invalid option");
+			}
+
+		}
+
+	}
+	
+	public static void menu() {
+		C206_CaseStudy.setHeader("Tuition Management System");
+		System.out.println("1. Maintain Tuition");
+		System.out.println("2. Registration of tuition timetable");
+		System.out.println("3. Maintain Tuition Timetable");
+		System.out.println("4. Maintain Student");
+		System.out.println("5. Quit");
+		Helper.line(80, "-");
+
+	}
+	
+	public static void setHeader(String header) {
+		Helper.line(80, "-");
+		System.out.println(header);
+		Helper.line(80, "-");
 	}
 
 }
---
+++ C206_CaseStudy/src/Tuition.java
@@ -0,0 +1,11 @@
+
+/**
+ * I declare that this code was written by me.
+ * I will not copy or allow others to copy my code.
+ * I understand that copying code is considered as plagiarism.
+ *
+ * brain, 4 Aug 2021 4:52:58 pm
+ */
+public class Tuition {
+
+}
